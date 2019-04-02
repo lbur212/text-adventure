0656cc0f24f8580de923ede4d51f854ffa7120cc
lbur212 <lbur212@learner.dvusd.org>
2019-04-02 13:31:59 -0700

    Added previous main menu work.

---
+++ textAdventure/Map.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace kak
+{
+    class Map
+    {
+        char[,] tiles = new char[37, 36];
+        string[,] ideologies = new string[37, 36];
+
+        public Map()
+        {
+            string line;
+            char[] tempArray = new char[37];
+            StreamReader sr = new StreamReader("U:\\Software Development\\Projects\\Visual Studio\\kak\\startmap.txt");
+            line = sr.ReadLine();
+            int count = 0;
+            while (line != null)
+            {
+                tempArray = line.ToCharArray();
+                for (int i = 0; i < 36; i++)
+                {
+                    tiles[count, i] = tempArray[i];
+                }
+
+                count++;
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+
+            for (int i = 0; i < 37; i++)
+            {
+                for (int j = 0; j < 36; j++)
+                {
+                    switch(tiles[i, j])
+                    {
+                        case '#':
+                            ideologies[i, j] = "fascism";
+                            break;
+                        case ';':
+                            ideologies[i, j] = "democracy";
+                            break;
+                        case 'X':
+                            ideologies[i, j] = "communism";
+                            break;
+                        case '?':
+                            ideologies[i, j] = "unaligned";
+                            break;
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string ret = "\ta kak political game\n\n";
+            ret += "_____________________________________\n";
+
+            for (int i = 0; i < 37; i++)
+            {
+                ret += "|";
+
+                for (int j = 0; j < 36; j++)
+                {
+                    ret += tiles[i, j];
+                }
+
+                ret += "|\n";
+            }
+
+            ret += "_____________________________________\n";
+
+            return ret;
+        }
+    }
+}
---
+++ textAdventure/Menu.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace kak
+{
+    class Menu
+    {
+        public string mainMenu()
+        {
+            Console.Clear();
+
+            string menu = "";
+            string line;
+            StreamReader sr = new StreamReader("U:\\Software Development\\Projects\\Visual Studio\\kak\\mainmenu.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                menu += line;
+                menu += "\n";
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+
+            return menu;
+        }
+
+        public string optionsMenu()
+        {
+            Console.Clear();
+
+            string menu = "";
+            string line;
+            StreamReader sr = new StreamReader("U:\\Software Development\\Projects\\Visual Studio\\kak\\optionsmenu.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                menu += line;
+                menu += "\n";
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+
+            return menu;
+        }
+
+        public void mainMenuInput(string i, Map t)
+        {
+            switch (i)
+            {
+                case "p":
+                    Console.Clear();
+                    Console.WriteLine(t);
+                    break;
+                case "o":
+                    Console.WriteLine(optionsMenu());
+                    optionsMenuInput(Console.ReadLine(), t);
+                    break;
+                case "e":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine(mainMenu());
+                    break;
+            }
+        }
+
+        public void optionsMenuInput(string i, Map t)
+        {
+            switch (i)
+            {
+                case "b":
+                    Console.WriteLine(mainMenu());
+                    mainMenuInput(Console.ReadLine(), t);
+                    break;
+                default:
+                    Console.WriteLine(optionsMenu());
+                    break;
+            }
+        }
+    }
+}
